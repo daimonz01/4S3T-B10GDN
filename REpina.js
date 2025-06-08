48d60f30271a8939be365a3c4691ccf6cd329a08
daimonz01 <44843350+daimonz01@users.noreply.github.com>
2025-06-08 21:33:12 +0700

    Update REpina.js

--- REpina.js
+++ REpina.js
@@ -39,6 +39,7 @@ const daftarPengecualian = [
   "/2024/01/reverse-1999-data-game-android.html",
   "/2024/12/starseed-asnia-trigger-data-game.html",
   "/2025/02/astra-knights-of-veda-data-game.html",  
+  "/2024/07/zenless-zone-zero-data-game-android.html",  
   
   "/jangan-redirect.html"
 ];
